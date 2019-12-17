45799a808f783759eb1203d7f593a53732d0058b
Damien Dube <damiendube@Damiens-MacBook-Pro.local>
2019-12-17 10:54:03 -0500

    Adding configs

--- application.py
+++ application.py
@@ -1,4 +1,20 @@
 from demo import application
+import click
+import click_config_file
+import toml
 
-if __name__ == "__main__":
-    application.run()
+
+def yaml_config_provider(file_path, cmd_name):
+    with open(file_path) as config_data:
+        return toml.load(config_data)
+
+
+@click.command()
+@click.argument('host')
+@click_config_file.configuration_option(provider=yaml_config_provider)
+def main(host):
+    application.run(host)
+
+
+if __name__ == '__main__':
+    main()
